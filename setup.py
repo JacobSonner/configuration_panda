875e77c62971ce8055255f0fbc7b880509aa5c9c
Mike Dunn <mike@eikonomega.com>
2014-01-13 21:52:17 -0500

    Add PyPI release integration for TravisCI.

--- setup.py
+++ setup.py
@@ -2,11 +2,15 @@ from setuptools import setup, find_packages
 
 import configuration_panda
 
+requirements = open('requirements.txt')
+
 setup(
     name="ConfigurationPanda",
     packages=find_packages(),
     version=configuration_panda.__version__,
     author="Mike Dunn",
-    author_email="mike@eikonomega.com"
+    author_email="mike@eikonomega.com",
+
+    install_requires=requirements
 )
 
