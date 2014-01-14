eca26b603360fe31c83170a4e8fd4e1cea6e46fe
Mike Dunn <mike@eikonomega.com>
2014-01-14 14:48:27 -0500

    Change package name, add url, description, and long_description attributes.

--- setup.py
+++ setup.py
@@ -2,18 +2,22 @@ from setuptools import setup, find_packages
 
 import configuration_panda
 
-requirements = open('requirements.txt').readlines()
+with open('requirements.txt') as requirements_doc:
+    requirements = requirements_doc.read()
 
 setup(
-    name="ConfigurationPanda",
+    name="configuration-panda",
     packages=find_packages(),
     version=configuration_panda.__version__,
     author="Mike Dunn",
     author_email="mike@eikonomega.com",
+    url="https://github.com/eikonomega/configuration_panda",
+    description="Easy program configuration for Pandas.",
+    long_description=configuration_panda.__doc__,
 
     install_requires=requirements,
     #include_package_data=True,
-    package_data = {
+    package_data={
         '': ['requirements.txt']
     }
 )
